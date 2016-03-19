6055f3a41c49e69f5dc2046ea1206338758fbe18
Jan Polacek <janpolacek@outlook.com>
2016-03-19 18:08:22 +0100

    gruntfile automatizacia

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,44 @@
 module.exports = function(grunt) {
+  var globalConfig = {
+    jan:{
+      src:'jan/src',
+      build:'jan/build'
+    },
+    global:{
+      src:'global'
+    }
+  };
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    globalConfig: globalConfig,
 
     sass: {
       dist: {
         files: {
-          'jan/build/mainformsstyles.css': 'jan/styles/mainformsstyles.scss',
-          'martin/homepage/build/homepage.css':'martin/homepage/styles/mainhomepagestyles.scss'
+          '<%= globalConfig.jan.build  %>/mainformsstyles.css': '<%= globalConfig.jan.src  %>/styles/mainformsstyles.scss',
+          '<%= globalConfig.global.src  %>/styles/global.css' : '<%= globalConfig.global.src  %>/styles/global.scss'
         }
       }
     },
     concat: {
       options: {
-        separator: ';',
+        separator: ';'
       },
       forms: {
         files:[
           {
-            src: ['jan/js/**/*.js', '!jan/js/base/load.js', 'jan/js/base/load.js'],
-              dest:'jan/build/forms.js'
-           },
-          {
-            src: ['martin/homepage/js/**/*.js'],
-            dest: 'martin/homepage/build/homepage.js'}
+            src: ['<%= globalConfig.jan.src  %>/js/**/*.js', '!<%= globalConfig.jan.src  %>/js/base/load.js', '<%= globalConfig.jan.src  %>/js/base/load.js'],
+              dest:'<%= globalConfig.jan.build  %>/forms.js'
+           }
         ]
       }
     },
     webfont: {
       icons: {
-        src: 'global/content/svg/*.svg',
-        dest: 'global/styles/fonts/',
-        destCss: 'global/styles/',
+        src: '<%= globalConfig.jan.src  %>/content/svg/*.svg',
+        dest: '<%= globalConfig.jan.src  %>/styles/fonts/',
+        destCss: '<%= globalConfig.jan.src  %>/styles/',
         options: {
             font:'font-icon',
              stylesheet: 'scss',
@@ -45,41 +53,55 @@ module.exports = function(grunt) {
     },
     mustache: {
       files : {
-        src: 'global/templates/',
-        dest: 'global/js/templates.js',
+        src: '<%= globalConfig.jan.src  %>/templates/',
+        dest: '<%= globalConfig.jan.build %>/templates.js',
         options: {
-          prefix: 'wm.__.templates = ',
+          prefix: '_WMGlobal.templates = ',
           postfix: ";",
           verbose:true,
           livereload: true
-
+    
         }
       }
     },
+    copy: {
+      main: {
+        expand: true,
+        cwd: '<%= globalConfig.jan.src %>/',
+        src: ['vendor/**/*','content/**/*'],
+        dest: '<%= globalConfig.jan.build %>/',
+      },
+    },
+    clean: ["<%= globalConfig.jan.build  %>"],
+
 
     watch: {
       css: {
         files: [
-          'jan/styles/**/*.scss',
-          'martin/homepage/styles/**/*.scss',
-          'global/styles/**/*.scss'],
-        tasks: ['sass'],
+          '<%= globalConfig.jan.src  %>/styles/**/*.scss',
+          '<%= globalConfig.global.src  %>/styles/**/*.scss'],
+        tasks: ['clean','sass','concat','mustache','copy'],
         options: {
           livereload: true
         }
       },
-      //mustache:{
-      //  files: ['**/*.mustache'],
-      //  tasks: ['mustache'],
-      //  options: {
-      //
-      //    livereload: true
-      //  }
-      //
-      //},
       js: {
-        files: ['jan/js/**/*.js','!global/js/templates.js','martin/homepage/js/**/*.js'],
-        tasks: ['concat'/*,'uglify'*/],
+        files: ['<%= globalConfig.jan.src  %>/js/**/*.js'],
+        tasks: ['clean','sass','concat','mustache','copy'],
+        options: {
+          livereload: true
+        }
+      },
+      mustache:{
+        files: ['<%= globalConfig.jan.src  %>/templates/**/*.mustache'],
+        tasks: ['clean','sass','concat','mustache','copy'],
+        options: {
+          livereload: true
+        }
+      },
+      other:{
+        files: ['<%= globalConfig.jan.src  %>/content/*','<%= globalConfig.jan.src  %>/vendor/*'],
+        tasks: ['clean','sass','concat','mustache','copy'],
         options: {
           livereload: true
         }
@@ -96,11 +118,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-mustache');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
 
-  grunt.registerTask('default',['watch']);
+  grunt.registerTask('default',['clean','sass','concat','mustache','copy','watch']);
   grunt.registerTask('dist', ['uglify', 'cssmin']);
   grunt.registerTask('font-build', ['webfont']);
+  grunt.registerTask('build', ['concat:dist', 'uglify:dist']);
+
 
 
 
---
+++ jan/build/templates.js
@@ -0,0 +1,3 @@
+_WMGlobal.templates = {
+    "done": "true"
+  };
